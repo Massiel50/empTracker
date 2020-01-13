8916cff24ae08200c119d81a19d965003d8e2e38
Massiel50 <massielescano@yahoo.com>
2020-01-13 12:19:09 -0500

    required mysql and inquirer, made connection var

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+var connection = mysql.createConnection(function(){
+    if (err) throw err;
+
+    startQuery();
+
+});
+
+function startQuery(){
+
+}
