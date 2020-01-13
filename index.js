257cf36e0f0cb249024116de411fce5329c84139
Massiel50 <massielescano@yahoo.com>
2020-01-13 15:02:39 -0500

    init connection, startquery func, switch to transition options

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-var connection = mysql.createConnection(function(){
-    if (err) throw err;
-
-    startQuery();
-
+var connection = mysql.createConnection({
+    host: "localhost",
+    PORT: 3306,
+    user: "root",
+    password: "",
+    database: ""
 });
 
+connection.connect(function(){
+    if(err) throw err;
+    console.log("connection initiated");
+
+    startQuery();
+})
+
 function startQuery(){
-    
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Select an action',
+            choices: [
+                "View all employee's", 
+                "View all employee's by dept", 
+                "View all employee's by manager", 
+                "add employee", 
+                "remove employee", 
+                "update employee role", 
+                "update employee manager",
+                ],
+            name: "selected"
+        }
+    ]).then(function(response){
+
+        switch(response){
+            case  "View all employee's":
+                viewAllEmp();
+                break;
+            case "View all employee's by dept":
+                viewAllDept();
+                break;
+            case "View all employee's by manager":
+                viewAllMangr();
+                break;
+            case "add employee":
+                addEmployee();
+                break;
+            case "remove employee":
+                remEmployee();
+                break; 
+            case "update employee role":
+                updEmplRole();
+                break;
+            case "update employee manager":
+                updEmplMangr();
+                break;
+        }
+    })
 }
